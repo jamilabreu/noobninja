9a236da045fa32b01cafd79a7f68fc897e5408ae
Jamil Abreu <abreu.jamil@gmail.com>
2012-10-08 01:27:53 -0400

    Migration fix

--- db/migrate/20121008043308_create_invites.rb
+++ db/migrate/20121008043308_create_invites.rb
@@ -1,7 +1,10 @@
 class CreateInvites < ActiveRecord::Migration
   def change
     create_table :invites do |t|
-
+      t.string :email
+      t.string :background
+      t.boolean :sensei, default: false
+      
       t.timestamps
     end
   end
