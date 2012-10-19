d7a4308a4c2ba4796adfb66c401148aac0e02e15
Jamil Abreu <abreu.jamil@gmail.com>
2012-10-18 20:32:03 -0400

    no email validation

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,6 +1,6 @@
 class Invite < ActiveRecord::Base
   attr_accessible :background, :email, :sensei
-  validates :email, :presence => true, :email => true
+  validates :email, :presence => true#, :email => true
   validates :background, :presence => true, :if => :background_needed?
 
   def background_needed?
